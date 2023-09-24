dc8b3fb3ea5e3c4d377f2b85b5eb3a2605fd42dc
edsonpessoalize <136597747+edsonpessoalize@users.noreply.github.com>
2023-09-24 12:57:45 -0300

    first commit true

---
+++ estruturas sequenciais/01.py
@@ -0,0 +1,7 @@
+"""
+fonte: https://wiki.python.org.br/EstruturaSequencial
+Estruturas Sequenciais
+01 - Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+"""
+
+print('Hello World!!!')
---
+++ estruturas sequenciais/01.rb
@@ -0,0 +1,7 @@
+=begin
+fonte: https://wiki.python.org.br/EstruturaSequencial
+Estruturas Sequenciais
+01 - Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+=end
+
+puts 'Hello World!!!'
